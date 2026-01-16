56e2ae450e97af8b0ec98fa345ed3cb2f589cf45
Kishore V <vkishore157@gmail.com>
2026-01-16 12:44:09 +0530

    Commit my Book class

---
+++ src/Book.java
@@ -0,0 +1,39 @@
+public class Book {
+   //encapsulation:making all the things that user can't able to modify from that main class
+    private int id;
+    private String title;
+    private String author;
+    private boolean issued;
+
+    Book(int id,String title,String author,boolean issued){
+        this.id=id;
+        this.title=title;
+        this.author=author;
+        this.issued=issued;
+    }
+    void get_id(){
+        System.out.println(this.id);
+    }
+    void get_title(){
+        System.out.println(this.title);
+    }
+    void get_author(){
+        System.out.println(this.author);
+    }
+    void set_id(int id){
+        this.id=id;
+    }
+    void set_title(String title){
+        this.title=title;
+    }
+    void set_author(String author){
+        this.author=author;
+    }
+    void set_issued(boolean issued ){
+        this.issued=issued;
+    }
+    @Override
+    public String toString(){
+       return "This is book title "+this.title+"\nThe author name is "+this.author+"\nAnd there id is "+this.id+"\nAnd is wether published "+this.issued;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Book b1 = new Book(1234565432, "kishore","THE ART OF LONELESS",false);
+        Book b2 = new Book(1786345634, "sree","THE ART OF LOve",true);
+        Book b3 = new Book(1645674567, "jojojo","THE ART OF pure",false);
+        Book b4 = new Book(1098654564, "jkjkjkjkj","THE ART OF range" ,true);
+
+        System.out.println(b1);
+        System.out.println(b1);
+        System.out.println(b1);
+        System.out.println(b1);
+    }
+}
